728d981f7382f617ec226b455b2991332d6ca089
richieaj84 <richiejames09@gmail.com>
2016-06-29 12:23:47 +0100

    New data integration job

---
+++ dbtest/dbtest/dbtest_run.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/commons-collections-3.2.2.jar;../lib/log4j-1.2.15.jar;../lib/log4j-1.2.16.jar;../lib/dom4j-1.6.1.jar;../lib/trove.jar;../lib/mysql-connector-java-5.1.30-bin.jar;../lib/advancedPersistentLookupLib-1.0.jar;../lib/jboss-serialization.jar;dbtest_0_1.jar; gittest2.dbtest_0_1.dbtest --context=Default %* 
